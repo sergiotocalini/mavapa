6aae8003db92fa3b13961b51320c4ec4add591ce
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2019-03-11 15:04:21 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,12 +3,22 @@ from werkzeug.serving import run_simple
 from werkzeug.wsgi import DispatcherMiddleware
 from mavapa import app
 
+
 def simple(env, resp):
     """A wrapper of a prefix"""
     resp(b'200 OK', [(b'Content-Type', b'text/plain')])
     return [b"Hello WSGI World"]
 
-dispatcher = DispatcherMiddleware(simple, {app.config.get('APPLICATION_ROOT', '/'): app}) # pylint: disable=I0011,C0103
+
+context = app.config.get('APPLICATION_ROOT', '/')
+dispatcher = DispatcherMiddleware(
+    simple, {context: app}
+)  # pylint: disable=I0011,C0103
+
 
 if __name__ == "__main__":
-    run_simple(app.config.get('BIND', '0.0.0.0'), app.config.get('PORT', 7001), dispatcher)
+    run_simple(
+        app.config.get('BIND', '0.0.0.0'),
+        app.config.get('PORT', 7001),
+        dispatcher
+    )
