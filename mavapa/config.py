710450a6acad542ed5ee6ece7ba230297207be87
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2024-08-02 21:43:01 +0200

    Adding changes

--- mavapa/config.py
+++ mavapa/config.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # Default configuration
+import os
 
 BIND = '0.0.0.0'
 PORT = 7001
 CSRF_ENABLED = True
-SECRET_KEY = 'WYmpcUG1UA2ORJgRJoYt'
+SECRET_KEY = os.urandom(24)
 APPLICATION_ROOT = ''
 DEBUG = False
+JSONIFY_PRETTYPRINT_REGULAR = False
 PONY_DEBUG = False
 PONY_GENERATE_MAPPING = dict(
-    create_tables  = True,
-    check_tables   = True
+    create_tables=True,
+    check_tables=True,
 )
+
 PONY = dict(
-    provider = "sqlite",
-    filename = ":memory:"
+    provider="sqlite",
+    filename=":memory:"
 )
-JSONIFY_PRETTYPRINT_REGULAR = False
